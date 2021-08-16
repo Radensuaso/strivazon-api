557e3b0442f5cf038b691c138a40e097eea358db
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-08-16 12:01:09 +0100

    get all

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,5 +1,35 @@
-import express from "express";
+import express from "express"
+import {
+  readProducts,
+  writeProducts,
+  saveProductPicture,
+  readProductsReviews,
+} from "../../lib/fs-tools.js"
+import uniqid from "uniqid"
+import createHttpError from "http-errors"
+import multer from "multer"
 
-const productsRouter = express.Router();
+const productsRouter = express.Router()
 
-export default productsRouter;
+productsRouter.get("/", async (req, res, next) => {
+  try {
+    const products = await readProducts()
+    console.log(products)
+
+    if (req.query && req.query.category) {
+      const filteredProducts = products.filter((product) =>
+        product.category
+          .toLocaleLowerCase()
+          .includes(req.query.category.toLocaleLowerCase())
+      )
+      res.send(filteredProducts)
+    } else {
+      res.send(products)
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+export default productsRouter
