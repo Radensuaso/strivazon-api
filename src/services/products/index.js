d8066113c3fa21f2e53e2c1d58c8c5259df3280d
Radensuaso <andrels9283@gmail.com>
2021-08-16 14:58:36 +0100

    Product picture uploaded

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,92 +1,92 @@
-import express from "express"
+import express from "express";
 import {
   readProducts,
   writeProducts,
   saveProductPicture,
   readProductsReviews,
-} from "../../lib/fs-tools.js"
-import { productsValidation } from "./validation.js"
-import { validationResult } from "express-validator"
-import uniqid from "uniqid"
-import createHttpError from "http-errors"
-import multer from "multer"
+} from "../../lib/fs-tools.js";
+import { productsValidation } from "./validation.js";
+import { validationResult } from "express-validator";
+import uniqid from "uniqid";
+import createHttpError from "http-errors";
+import multer from "multer";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const products = await readProducts()
-    console.log(products)
+    const products = await readProducts();
+    console.log(products);
 
     if (req.query && req.query.category) {
       const filteredProducts = products.filter((product) =>
         product.category
           .toLocaleLowerCase()
           .includes(req.query.category.toLocaleLowerCase())
-      )
-      res.send(filteredProducts)
+      );
+      res.send(filteredProducts);
     } else {
-      res.send(products)
+      res.send(products);
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 productsRouter.get("/:_id", async (req, res, next) => {
   try {
-    const paramsID = req.params._id
-    const products = await readProducts()
-    const product = products.find((p) => p._id === paramsID)
+    const paramsID = req.params._id;
+    const products = await readProducts();
+    const product = products.find((p) => p._id === paramsID);
     if (product) {
-      res.send(product)
+      res.send(product);
     } else {
       res.send(
         createHttpError(
           404,
           `The Product with the id: ${paramsID} was not found.`
         )
-      )
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productsRouter.get("/:_id/productsReviews", async (req, res, next) => {
   try {
-    const paramsID = req.params._id
-    const products = await readProducts()
-    const product = products.find((p) => p._id === paramsID)
+    const paramsID = req.params._id;
+    const products = await readProducts();
+    const product = products.find((p) => p._id === paramsID);
     if (product) {
-      const productsReviews = await readProductsReviews()
+      const productsReviews = await readProductsReviews();
 
-      console.log(productsReviews)
+      console.log(productsReviews);
 
       const particularProductReviews = productsReviews.filter(
         (p) => p.productId === paramsID
-      )
-      res.send(particularProductReviews)
+      );
+      res.send(particularProductReviews);
     } else {
       res.send(
         createHttpError(
           404,
           `The Product with the id: ${paramsID} was not found.`
         )
-      )
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productsRouter.post("/", productsValidation, async (req, res, next) => {
   try {
-    const errorList = validationResult(req)
+    const errorList = validationResult(req);
     if (errorList.isEmpty()) {
-      const products = await readProducts()
-      const reqBody = req.body
+      const products = await readProducts();
+      const reqBody = req.body;
 
       const newProduct = {
         _id: uniqid(),
@@ -98,18 +98,51 @@ productsRouter.post("/", productsValidation, async (req, res, next) => {
         category: reqBody.category,
         createdAt: new Date(),
         updatedAt: new Date(),
-      }
-      products.push(newProduct)
-      await writeProducts(products)
+      };
+      products.push(newProduct);
+      await writeProducts(products);
 
-      res.status(201).send(newProduct)
+      res.status(201).send(newProduct);
     } else {
-      next(createHttpError(400, { errorList }))
+      next(createHttpError(400, { errorList }));
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
-export default productsRouter
+productsRouter.post(
+  "/:_id/upload",
+  multer().single("picture"),
+  async (req, res, next) => {
+    try {
+      const paramsId = req.params._id;
+      const products = await readProducts();
+      const product = products.find((p) => p._id === paramsId);
+      if (product) {
+        await saveProductPicture(`${product._id}.jpg`, req.file.buffer);
+        const pictureUrl = `http://${req.get("host")}/img/productsPictures/${
+          product._id
+        }.jpg`;
+        const remainingProducts = products.filter((p) => p._id !== paramsId);
+        const updatedProduct = { ...product, imageUrl: pictureUrl };
+
+        remainingProducts.push(updatedProduct);
+        await writeProducts(remainingProducts);
+        res.send("Picture uploaded!");
+      } else {
+        next(
+          createHttpError(
+            404,
+            `The Product with the id: ${paramsId} was not found.`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default productsRouter;
